4e86bb727b073ef40d0d02808612eb26c0231791
sleepypillowz <alexacojido@gmail.com>
2023-04-15 20:47:57 +0800

    finalized
    bugs:
    When user doesn't guess the correct letter the "Letter is Wrong!" loops every even number.

--- STI/CP2/Java/word-guess/src/main/java/cp2/word/guess/Main.java
+++ STI/CP2/Java/word-guess/src/main/java/cp2/word/guess/Main.java
@@ -1,49 +1,73 @@
+//note if file is being written to other txt it can't print here in console.
 package cp2.word.guess;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
- 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    // to handle exceptions include throws
-    public static void main(String[] args)
-        throws IOException
-    {
-        // list that holds strings of a file
-        List<String> listOfStrings
-            = new ArrayList<String>();
-       
-        // load data from file
-        BufferedReader bf = new BufferedReader(
-            new FileReader("C:\\Users\\alexa\\OneDrive\\Documents\\GitHub\\school-code\\STI\\CP2\\Java\\word-guess\\source.txt"));
-       
-        // read entire line as string
-        String line = bf.readLine();
-       
-        // checking for end of file
-        while (line != null) {
-            listOfStrings.add(line);
-            line = bf.readLine();
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("C:\\Users\\alexa\\OneDrive\\Documents\\GitHub\\school-code\\STI\\CP2\\Java\\word-guess\\words.txt");
+        Scanner sf = new Scanner(file);
+        Scanner s = new Scanner(System.in);
+        String fileContent = "";
+        String[] fileContentArray = new String[54];
+        int min = 1;
+        int max = 53;
+        int range = max - min + 1;
+        int randomNum = (int) Math.floor(Math.random() * range + min);
+        String randomWord;
+        char ans;
+        int same = 1;
+
+        while (sf.hasNextLine()) {
+
+            for (int i = 0; i < fileContentArray.length; i++) {
+                fileContentArray[i] = fileContent.concat(sf.nextLine());
+            }
         }
-       
-        // closing bufferreader object
-        bf.close();
-       
-        // storing the data in arraylist to array
-        String[] array
-            = listOfStrings.toArray(new String[0]);
-       
-        
-        for (int i = 0; i < array.length i++) {
-            
+
+        randomWord = fileContentArray[randomNum];
+
+        char[] randomWordArray = randomWord.toCharArray();
+        char[] jumbledRandomWordArray = randomWord.toCharArray();
+
+        for (int j = 0; j < jumbledRandomWordArray.length; j++) {
+            if (j % 2 != 0) {
+                jumbledRandomWordArray[j] = '?';
+
+            }
         }
-        
-        // printing each line of file
-        // which is stored in array
-        for (String str : array) {
-            System.out.println(str);
+
+        while (same != 0) {
+            same = Arrays.compare(randomWordArray, jumbledRandomWordArray);
+            System.out.println(jumbledRandomWordArray);
+
+            if (same != 0) {
+
+                System.out.print("Input a Letter: ");
+                ans = s.next().charAt(0);
+                for (int k = 0; k < jumbledRandomWordArray.length; k++) {
+
+                    if (k % 2 != 0) {
+                        if (ans == randomWordArray[k]) {
+                            jumbledRandomWordArray[k] = ans;
+                        }
+
+                        if (ans != randomWordArray[k]) {
+                            System.out.println("x");
+                        }
+                    }
+                }
+            }
+
+            if (same == 0) {
+                System.out.println("You won!");
+            }
         }
+
     }
 }
