ea49c43b94b4fe04edc2297119764e932f08735f
sleepypillowz <alexacojido@gmail.com>
2023-04-15 18:17:22 +0800

    directly adds file read to an array

--- STI/CP2/Java/word-guess2/src/main/java/cp2/word/guess2/Main.java
+++ STI/CP2/Java/word-guess2/src/main/java/cp2/word/guess2/Main.java
@@ -9,38 +9,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        
-        //File Location
+
         File file = new File("C:\\Users\\alexa\\OneDrive\\Documents\\GitHub\\school-code\\STI\\CP2\\Java\\word-guess2\\words.txt");
-
-        //Reads the File
         Scanner s = new Scanner(file);
-        
-
-
-        //Print to Console the 1st Line
-        //System.out.println(s.nextLine());
-        //String storage for fileContent
         String fileContent = "";
-        //Array storage for fileContent
-        String[] fileContentArray = {};
+        String[] fileContentArray = new String[54];
 
-        //Loops until all is printed to the console
         while (s.hasNextLine()) {
-            //System.out.println(s.nextLine());
 
-            //puts fileContent to storage
-            fileContent = fileContent.concat(s.nextLine() + "\n");
+            for (int i = 0; i < fileContentArray.length; i++) {
+                fileContentArray[i] = fileContent.concat(s.nextLine());
+            }            
         }
-        //puts String fileContent to an Array
-        fileContentArray = fileContent.split(" ", -5);
 
-        //loops til all elements of an array is printed
-        
-        for (int i = 0; i < fileContentArray.length; i++) {
-            System.out.println(fileContentArray[i]);
-        }
-        
-        System.out.println(fileContentArray.length);
+        System.out.println(fileContentArray[53]);
     }
 }
