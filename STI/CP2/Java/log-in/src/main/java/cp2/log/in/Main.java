1fd9bdaa9d3ebe43b314122d23cfbb3a4a4baaed
sleepypillowz <alexacojido@gmail.com>
2023-04-16 09:24:19 +0800

    initial commit

--- STI/CP2/Java/log-in/src/main/java/cp2/log/in/Main.java
+++ STI/CP2/Java/log-in/src/main/java/cp2/log/in/Main.java
@@ -1,66 +1,92 @@
 package cp2.log.in;
 
-import java.nio.file.*;
-import java.io.*;
-import static java.nio.file.StandardOpenOption.*;
-import java.text.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-                
-        //example 8 StandardOpenOption Arguments
-        /*
-        Path file = Paths.get("C:\\Users\\alexa\\OneDrive\\Documents\\GitHub\\school-code\\STI\\CP2\\Java\\log-in\\first_name.txt");
-        String s = "RANIKA";
-        byte[] data = s.getBytes();
-        OutputStream output = null;
-        try {
-            output = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
-            output.write(data);
-            output.flush();
-            output.close();
-        }
-        catch (Exception e) {
-            System.out.println("Messsage: " + e);
-        }
-        */
-        
-        //example 9 BuffedWriter Methods
-        
-        Scanner input = new Scanner(System.in);
-        Path file = Paths.get("C:\\Users\\alexa\\OneDrive\\Documents\\GitHub\\school-code\\STI\\CP2\\Java\\log-in\\grades.txt");
-        String s = "";
-        String delimiter = ",";
-        long id;
-        String name;
-        double grade;
-        final long QUIT = 0;
-        try {
-            OutputStream output = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
-            BufferedWriter writer = new
-        BufferedWriter(new OutputStreamWriter(output));
-            System.out.print("Enter student iD number: ");
-            id = input.nextLong();
-            while(id != QUIT) {
-                System.out.print("Enter name for student no. " + id + ": ");
-                input.nextLine();
-                name = input.nextLine();
-                System.out.print("Enter the subject grade: ");
-                grade = input.nextDouble();
-                DecimalFormat df = new DecimalFormat("0.00");
-                s = id + delimiter + name + delimiter + df.format(grade);
-                writer.write(s, 0, s.length());
-                writer.newLine();
-                System.out.print("Enter next ID number or " + QUIT + " to quit: ");
-                id = input.nextLong();
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        File file = new File("C:\\Users\\alexa\\OneDrive\\Documents\\GitHub\\school-code\\STI\\CP2\\Java\\log-in\\records.txt");
+        Scanner sf = new Scanner(file);
+        Scanner s = new Scanner(System.in);
+        int choice;
+        String username = "";
+        String password = "";
+        String registeredUsername = "";
+        String registeredPassword = "";
+        String[] fileContentArray = new String[2];
+        String login;
+
+        String fileContent = "";
+
+        while (sf.hasNextLine()) {
+
+            for (int i = 0; i < fileContentArray.length; i++) {
+                fileContentArray[i] = fileContent.concat(sf.nextLine());
+                System.out.println(fileContentArray[i]);
             }
-            writer.close();
         }
-        catch(Exception e) {
-            System.out.println("Message: " + e);
-        }
+        registeredUsername = fileContentArray[0];
+        registeredPassword = fileContentArray[1];
+
+        System.out.println("Would you like to Register or Login?\nPress 0 for Register\nPress 1 for Login");
+        try {
+        choice = s.nextInt();
         
+            if (choice == 0) {
+
+                System.out.println("REGISTER");
+                System.out.print("Enter Username: ");
+                registeredUsername = s.next();
+                System.out.print("Enter Password: ");
+                registeredPassword = s.next();
+
+                FileWriter writer = new FileWriter("C:\\Users\\alexa\\OneDrive\\Documents\\GitHub\\school-code\\STI\\CP2\\Java\\log-in\\records.txt");
+                writer.write(registeredUsername + "\n");
+                writer.write(registeredPassword);
+                writer.close();
+
+                System.out.println("Registered!");
+                System.out.println("Welcome, " + registeredUsername);
+                System.out.println("Your Password is: " + registeredPassword);
+                System.out.println("Would you like to login?\nenter y or n");
+                login = s.next();
+                if ("y".equals(login)) {
+                    choice = 1;
+                } else {
+                    System.out.println("Bye!");
+                }
+
+            }
+            if (choice == 1) {
+                while (!username.equals(registeredUsername) && !password.equals(registeredPassword)) {
+                    System.out.println("LOGIN");
+                    System.out.print("Enter Username: ");
+                    username = s.next();
+                    System.out.print("Enter Password: ");
+                    password = s.next();
+                    if (username.equals(registeredUsername) && password.equals(registeredPassword)) {
+                        System.out.println("Logged in!");
+                    } else {
+                        System.out.println("Wrong username or psssword.");
+                    }
+                }
+
+            }
+            if (choice != 0 && choice != 1) {
+                throw new Exception("Out of range, Try again.");
+
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid Input!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
